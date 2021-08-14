7c9a70178e1e1eef213855b5e006a6e6712afc3c
andy <jia470079834@gmail.com>
2021-08-14 17:16:17 -0400

    user auth helper auth token setup

--- Backend/routers/users.js
+++ Backend/routers/users.js
@@ -39,6 +39,24 @@ router.post('/', async (req, res)=>{
     res.send(user);
 });
 
+router.post('/register', async (req, res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
+    if (!user) return res.status(500).send('the user cannot be created!')
+    res.send(user);
+});
+
 router.post('/login', async (req, res) => {
     const user = await User.findOne({email: req.body.email})
     if(!user){
@@ -60,5 +78,6 @@ router.post('/login', async (req, res) => {
         res.status(400).send('Invalid password')
     }
 })
+
 
 module.exports = router;
