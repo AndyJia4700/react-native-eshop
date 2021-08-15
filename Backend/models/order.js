8c868ae4e0c3049699e520b7d034541b900bf381
andy <jia470079834@gmail.com>
2021-08-15 18:20:30 -0400

    order model setup

--- Backend/models/order.js
+++ Backend/models/order.js
@@ -1,7 +1,59 @@
 const mongoose = require('mongoose');
 
+
+//product => order => orderItems
 const orderSchema = mongoose.Schema({
-    
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: true,
+    }], //wrap with [] to make multiple order
+    shippingAddress1: {
+        type: String,
+        required: true,
+    },
+    shippingAddress2: {
+        type: String,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    zip: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: "Pending"
+    },
+    totalPrice: {
+        type: Number,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+orderSchema.method('toJSON', function(){
+    const { __v, ...object } = this.toObject();
+    const { _id:id, ...result } = object;
+    return { ...result, id }
 })
 
 exports.Order = mongoose.model('Order', orderSchema);
